815f40b0669a501db314cdab53f423ff7773f6d4
ben <ben@turingm.io>
2025-08-04 15:41:04 +0800

    fix： build issue

--- app/plugins/config.plugin.ts
+++
@@ -1,6 +0,0 @@
-import { initConfig } from "@/services/configService";
-// plugins/config.plugin.ts
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig()
-  initConfig(config.public)
-})
