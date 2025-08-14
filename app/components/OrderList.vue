be25805c9f22b0d0e5053226e52cdcb40e8c81e0
ben <ben@turingm.io>
2025-08-14 17:11:51 +0800

    add openorder and history list

--- app/components/OrderList.vue
+++ app/components/OrderList.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-tabs :active="active">
+  <van-tabs :active="active" :before-change="beforeChange">
     <van-tab title="Positions">
       <div
         class="w-full bg-color-white p-4 h-[calc(100vh-80px-248px-50px)] overflow-auto"
@@ -27,8 +27,60 @@
         </van-card>
       </div>
     </van-tab>
-    <van-tab title="Open orders">Open orders</van-tab>
-    <van-tab title="History">History</van-tab>
+    <van-tab title="Open orders">
+      <div
+        class="w-full bg-color-white p-4 h-[calc(100vh-80px-248px-50px)] overflow-auto"
+        v-if="openOrderList.length !== 0"
+      >
+        <van-card
+          v-for="item in openOrderList"
+          currency="$"
+          :key="item.marketId"
+          :price="item.profit + '(' + item.profitRate + '%)'"
+          :desc="item.description"
+          :title="item.question"
+          :thumb="item.image"
+          @click="goToDetails(item)"
+        >
+          <template #footer>
+            <van-button
+              plain
+              size="mini"
+              type="primary"
+              @click="showShares(item)"
+              >Shares
+            </van-button>
+          </template>
+        </van-card>
+      </div></van-tab
+    >
+    <van-tab title="History">
+      <div
+        class="w-full bg-color-white p-4 h-[calc(100vh-80px-248px-50px)] overflow-auto"
+        v-if="historyList.length !== 0"
+      >
+        <van-card
+          v-for="item in historyList"
+          currency="$"
+          :key="item.marketId"
+          :price="item.profit + '(' + item.profitRate + '%)'"
+          :desc="item.description"
+          :title="item.question"
+          :thumb="item.image"
+          @click="goToDetails(item)"
+        >
+          <template #footer>
+            <van-button
+              plain
+              size="mini"
+              type="primary"
+              @click="showShares(item)"
+              >Shares
+            </van-button>
+          </template>
+        </van-card>
+      </div></van-tab
+    >
   </van-tabs>
 
   <van-dialog v-model:show="show" title="SHARE TO" confirmButtonText="Confirm">
@@ -60,8 +112,12 @@
 </template>
 
 <script setup lang="ts">
-import { userHoldInfoList } from "@/api/positions";
-import { formatTitle } from '@/utils/processing'
+import {
+  userHoldInfoList,
+  userOrderList,
+  userTradeInfo,
+} from "@/api/positions";
+import { formatTitle } from "@/utils/processing";
 import { onMounted } from "vue";
 
 let positionList = $ref([]);
@@ -100,6 +156,46 @@ const fetchUserHoldInfoList = async () => {
   voState.isLoading = false;
 };
 
+/**
+ * load user open orders
+ */
+const fetchOpenOrderList = async () => {
+  try {
+    const res = await userOrderList(voState.queryParams);
+    if (voState.queryParams.pageNo === 1) {
+      openOrderList = res.data.list;
+    } else {
+      openOrderList = openOrderList.concat(res.data.list);
+    }
+    voState.total = res.data.total;
+    voState.isLoading = false;
+  } catch (error) {
+    //console.log(error)
+    voState.isLoading = false;
+  }
+};
+
+/**
+ * load user history orders
+ */
+const fetchHistoryList = async () => {
+  try {
+    const res = await userTradeInfo(voState.queryParams);
+    // .then(res => {
+    if (voState.queryParams.pageNo === 1) {
+      historyList = res.data.list;
+    } else {
+      historyList = historyList.concat(res.data.list);
+    }
+    voState.total = res.data.total;
+    voState.isLoading = false;
+    // })
+  } catch (error) {
+    //console.log(error)
+    voState.isLoading = false;
+  }
+};
+
 const goUrl = (url) => {
   let path = `${url}?url=${origin}?inviteCode=${userInfo.inviteCode}&text=Get Start With Turing Market`;
   origin = `${origin}/marketsDetails`;
@@ -110,7 +206,24 @@ const goUrl = (url) => {
 
 const goToDetails = (item) => {
   const title = formatTitle(item.question);
-  window.open(`https://avax-test.turingmarket.cc/details/${title}?id=${item.marketId}`);
+  window.open(
+    `https://avax-test.turingmarket.cc/details/${title}?id=${item.marketId}`
+  );
+};
+
+const beforeChange = (index) => {
+  active = index;
+  switch (index) {
+    case 0:
+      fetchUserHoldInfoList();
+      break;
+    case 1:
+      fetchOpenOrderList();
+      break;
+    case 2:
+      fetchHistoryList();
+      break;
+  }
 };
 
 onMounted(() => {
