b8cf74b2c4f747ca6f4b99dc8d72af5b88aa0915
ben <ben@turingm.io>
2025-08-22 09:56:50 +0800

    transcation and activity update

--- app/stores/walletStore.ts
+++ app/stores/walletStore.ts
@@ -45,7 +45,7 @@ export const walletStore = defineStore("walletStore", () => {
 
   const { $wagmiAdapter } = useNuxtApp();
   const { open } = useAppKit();
-  const { isConnected, address } = $(useAccount());
+  const { isConnected } = $(useAccount());
 
   const userCapital = $ref({
     total: 0,
@@ -195,7 +195,7 @@ export const walletStore = defineStore("walletStore", () => {
     const result = await readContract(config, {
       abi: market,
       address: coinInfo.address,
-      args: [address, walletConfig!.contract.address],
+      args: [wallet.address, walletConfig!.contract.address],
       functionName: "allowance",
     });
     return result as bigint;
@@ -254,12 +254,12 @@ export const walletStore = defineStore("walletStore", () => {
         const nonce = (await readContract(config, {
           abi: market,
           address: coinInfo.address,
-          args: [address],
+          args: [wallet.address],
           functionName: "nonces",
         })) as bigint;
         // Query nonce first
         const param = {
-          owner: address,
+          owner: wallet.address,
           spender: walletConfig!.contract.address,
           value: minValue,
           nonce: nonce,
@@ -275,7 +275,7 @@ export const walletStore = defineStore("walletStore", () => {
           type: coinType,
           sign: permitSig,
           deadline: param.deadline,
-          owner: address,
+          owner: wallet.address,
           spender: walletConfig!.contract.address,
         };
         const res = await approveSign(approveParam);
