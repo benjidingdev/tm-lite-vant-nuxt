1f361892ba7401564e1d6d42394b15559c9002ff
john <john@turingm.io>
2025-08-27 19:56:16 +0800

    add x invite view

---
+++ app/layouts/x.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="w-screen h-screen overflow-y-scroll text-white bg">
+      <slot />
+  </section>
+</template>
+
+<style scoped>
+.bg {
+  background: #000;
+  background-image: radial-gradient(ellipse 100% 70% at 50% -10%, #00ff9936, transparent);
+}
+</style>
