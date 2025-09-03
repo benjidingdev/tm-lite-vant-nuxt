87eea0cced32770417de07d638d3513500c6d9fd
john <john@turingm.io>
2025-09-03 17:06:22 +0800

    new share invite

---
+++ app/components/Invite/NewUserNotice.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <van-notice-bar left-icon="volume-o" :scrollable="false">
+    <van-swipe vertical class="notice-swipe h-[40px] leading-10" :autoplay="3000" :touchable="false"
+      :show-indicators="false">
+      <van-swipe-item>User1 got 1000 TUIT invited AAA successfully!</van-swipe-item>
+      <van-swipe-item>User1 got 1000 TUIT invited BBB successfully!</van-swipe-item>
+      <van-swipe-item>User1 got 1000 TUIT invited CCC successfully!</van-swipe-item>
+    </van-swipe>
+  </van-notice-bar>
+</template>
---
+++ app/pages/invite/guide.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { token } = $(authStore());
+let { fatherInviteCode } = $(userStore());
+
+definePageMeta({
+  layout: "invite",
+});
+
+onMounted(async () => {
+  console.log({fatherInviteCode});
+})
+</script>
+
+<template>
+  <InviteAccountShare v-if="token.accessToken && true" />
+  <InviteNoAccount v-else />
+</template>
