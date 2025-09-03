95ee708e423cf4b263b25b4d364256cf5b527296
albert <albertzhang@epclouds.com>
2025-09-03 18:04:57 +0800

    1

--- app/components/Balance/NavButton.vue
+++ app/components/Balance/NavButton.vue
@@ -5,7 +5,7 @@
   const { usdcBalance } = $(walletStore());
 
   const avatar = computed(() => {
-    return userInfo?.profile?.avatar || '/icons/usdc.svg'
+    return userInfo?.profile?.avatar || '/icons/usdt.svg'
   })
 </script>
 
