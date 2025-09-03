952cbd3a3fd3df92b7e27f12e41272bd336d0ef7
ben <ben@turingm.io>
2025-09-03 17:43:46 +0800

    withdraw logic

--- app/components/Balance/Withdraw.vue
+++ app/components/Balance/Withdraw.vue
@@ -1,62 +1,104 @@
 <script setup lang="ts">
-  import { withdrawRequest } from "@/api/wallet";
-  import { parseUnits } from "viem";
+import { withdrawRequest } from "@/api/wallet";
+import { getWithdrawSign } from "@/api/transcation";
+import { parseUnits } from "viem";
 
-  const { wallet, publicClient } = $(privyStore());
-  const { signWithdraw, walletConfig, userBalance } = $(walletStore());
-  const { getNonce } = $(authStore());
+const { wallet, publicClient } = $(privyStore());
+const { signWithdraw, walletConfig, userBalance } = $(walletStore());
+const { getNonce } = $(authStore());
+const { withdrawData } = $(withdrawStore());
+let depositHash = $ref("");
+let completionHash = ref("");
+let seconds = $ref(30);
+let timer = null;
+const activeNames = $ref(["1"]);
+let tokenAmount = $ref(1);
+let currentStep = $ref(1);
+let status = $ref("processing");
 
-  // let toAddress = $ref("");
-  let tokenAmount = $ref(1);
-  let currentStep = $ref(1);
-  let status = $ref("processing");
-  let toAddress = $ref('');
-
-  const waitTransaction = async (hash: string) => {
-    try {
-      const receipt = await publicClient.waitForTransactionReceipt({
-        hash,
-      });
-      if (receipt && receipt.status === "success") {
-        return receipt.transactionHash;
-      } else {
-        throw new Error("Transaction failed");
-      }
-    } catch (error) {
-      console.log("waitTransaction error:", error);
-      throw error;
+const startCountdown = () => {
+  timer = setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+    } else {
+      clearInterval(timer);
     }
-  };
+  }, 1000);
+};
 
-  const newWithdrawal = () => {
-    currentStep = 1;
-  };
+const waitTransaction = async (hash: string) => {
+  try {
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash,
+    });
+    if (receipt && receipt.status === "success") {
+      return receipt.transactionHash;
+    } else {
+      throw new Error("Transaction failed");
+    }
+  } catch (error) {
+    console.log("waitTransaction error:", error);
+    throw error;
+  }
+};
 
-  const withdraw = async (form) => {
-    const { toAddress } = form;
+const newWithdrawal = () => {
+  currentStep = 1;
+  resetForm();
+};
+
+const waitTx = async () => {
+  try {
+    const hash = await waitTransaction(depositHash);
+    if (hash) {
+      completionHash = hash;
+    }
+  } catch (e) {
+    console.error("waitTx ", e);
+  } finally {
+    resetTimer();
+  }
+};
+
+const withdraw = async (form) => {
+  try {
+    status = "processing";
+    startCountdown();
+    const { toAddress, tokenAmount } = withdrawData;
     currentStep = 2;
     // get a new nonce here
-    const { data: nonce } = await getNonce(wallet.address);
-    const amount_ = 10; // amount in USDC
-    const signParams = {
-      from: wallet.address,
+    const res = await getWithdrawSign({
+      amount: tokenAmount,
       to: toAddress,
-      amount: parseUnits(amount_.toString(), 6),
-      tokenAddress: walletConfig!.main.address,
-      nonce,
-    };
-    console.log(signParams, "signParams");
-    const userSign = await signWithdraw(signParams);
-    const requestParams = {
-      toAddress,
-      amount_,
-      nonce,
-      userSign,
-    };
-    const hash = withdrawRequest(requestParams);
-    const tx = await waitTransaction(hash);
-    console.log(tx);
-  };
+    });
+    if (res.code !== 0) return;
+
+    res.data.amount = parseUnits(res.data.amount.toString(), 6);
+    const signData = await signWithdraw(res.data);
+    if (!signData) return;
+
+    const rs = await withdrawRequest({
+      nonce: res.data.nonce,
+      userSign: signData,
+    });
+    if (rs.code != 0) return;
+    depositHash = rs.data;
+    await waitTx();
+    status = "success";
+  } catch (error) {
+    status = "failed";
+    resetTimer();
+  }
+};
+const resetTimer = () => {
+  clearInterval(timer);
+  seconds = 30;
+};
+
+const resetForm = () => {
+  withdrawData.toAddress = "";
+  withdrawData.tokenAmount = 0;
+};
 </script>
 
 <template>
@@ -65,20 +107,34 @@
       <van-form @submit="withdraw">
         <van-field name="toAddress">
           <template #input>
-            <BalanceForm v-model="toAddress" :maxlength="42" label="Recipient address" name="toAddress"
-              placeholder="0x..." />
+            <BalanceForm
+              v-model="withdrawData.toAddress"
+              :maxlength="42"
+              label="Recipient address"
+              name="toAddress"
+              placeholder="0x..."
+            />
           </template>
         </van-field>
         <van-field name="tokenAmount">
           <template #input>
-            <BalanceForm v-model="tokenAmount" label="Amount" name="tokenAmount" placeholder="0.00">
+            <BalanceForm
+              v-model="withdrawData.tokenAmount"
+              label="Amount"
+              name="tokenAmount"
+              placeholder="0.00"
+            >
               <template #input-right>
-                <div class="absolute right-1 top-1/2 transform -translate-y-1/2 flex items-center space-x-2">
+                <div
+                  class="absolute right-1 top-1/2 transform -translate-y-1/2 flex items-center space-x-2"
+                >
                   <span class="text-gray-500 font-medium">USDC</span>
                   <button
                     class="px-3 bg-blue-50 text-blue-600 text-xs font-semibold rounded-md hover:bg-blue-100 transition-colors border border-blue-200"
-                    type="button" @click="tokenAmount = userBalance">
-                    MAX
+                    type="button"
+                    @click="tokenAmount = userBalance"
+                  >
+                    {{ $t("MAX") }}
                   </button>
                 </div>
               </template>
@@ -86,7 +142,8 @@
                 <div class="flex justify-between space-x-2">
                   <span></span>
                   <!-- <span class="text-gray-400 font-medium text-xs">${{ tokenAmount }}</span> -->
-                  <span class="text-gray-400 text-xs ml-2">Balance:{{ userBalance }}
+                  <span class="text-gray-400 text-xs ml-2"
+                    >{{ $t("Balance") }}:{{ userBalance }}
                   </span>
                 </div>
               </template>
@@ -94,39 +151,87 @@
           </template>
         </van-field>
         <van-cell>
-          <van-button class="rounded-lg" block type="primary" native-type="submit">
-            {{ $t('withdraw') }}
+          <van-button
+            class="rounded-lg"
+            block
+            type="primary"
+            native-type="submit"
+          >
+            {{ $t("Withdraw") }}
           </van-button>
         </van-cell>
       </van-form>
     </div>
     <div v-if="currentStep === 2" class="step-two w-full">
-      <van-cell-group>
-        <van-cell>
-          <van-count-down :time="time">
-            <template #default="timeData">
-              <span class="block">{{ timeData.hours }}</span>
-              <span class="colon">:</span>
-              <span class="block">{{ timeData.minutes }}</span>
-              <span class="colon">:</span>
-              <span class="block">{{ timeData.seconds }}</span>
-            </template>
-          </van-count-down>
-        </van-cell>
+      <div>
+        <div
+          v-if="status === 'processing'"
+          class="flex justify-center items-center flex-col mt-5"
+        >
+          <van-circle
+            :current-rate="seconds"
+            size="40px"
+            :rate="60"
+            :speed="100"
+            layer-color="#f3f3f3"
+            :text="seconds + 's'"
+          />
+          <div class="text-center my-5">
+            <p class="text-sm">{{ $t("Submitting transaction") }}...</p>
+            <p class="text-center text-gray-500 text-xs">
+              {{ $t("Filling your transaction on the blockchain") }}
+            </p>
+          </div>
+        </div>
+        <div
+          v-else-if="status === 'success'"
+          class="min-h-[100px] flex justify-center items-center text-green-500"
+        >
+          <van-icon name="passed" size="28" />
+        </div>
+        <div
+          v-else
+          class="min-h-[100px] flex justify-center items-center text-red-500"
+        >
+          <van-icon name="close" size="50" />
+        </div>
+
         <van-cell title="Fill status" :value="status" />
-        <van-cell title="You receive" value="1.0007" />
-        <van-cell title="Transcation ID" value="0x123qsdq123sdad" />
+        <van-cell title="You receive" :value="tokenAmount" />
+        <van-collapse v-model="activeNames">
+          <van-collapse-item name="1">
+            <template #title>
+              <div class="text-xs leading-6 text-gray-400">
+                {{ $t("More Details") }}
+              </div>
+            </template>
+            <div class="flex justify-between text-xs text-gray-400">
+              <span class="leading-6">{{ $t("Deposit tx") }}</span>
+              <span>{{ toAddress || "0x...." }}</span>
+            </div>
+            <div class="flex justify-between text-xs text-gray-400">
+              <span>{{ $t("Order submitted") }}</span>
+              <span>time</span>
+            </div>
+          </van-collapse-item>
+        </van-collapse>
         <van-notice-bar color="#a7a7a7" background="#f9f9f9" left-icon="info-o">
-          <span class="font-xs">Experiencing problems?</span> <a>Get help</a>
+          <span class="font-xs">{{ $t("Experiencing problems") }}?</span>
+          <a>{{ $t("Get help") }}</a>
         </van-notice-bar>
-      </van-cell-group>
+      </div>
       <van-cell>
         <div class="flex mt-2 gap-2">
           <van-button block type="primary" plain native-type="submit">
-            Close
+            {{ $t("Close") }}
           </van-button>
-          <van-button block type="primary" native-type="submit" @click="newWithdrawal">
-            New Withdrawal
+          <van-button
+            block
+            type="primary"
+            native-type="submit"
+            @click="newWithdrawal"
+          >
+            {{ $t("New Withdrawal") }}
           </van-button>
         </div>
       </van-cell>
@@ -135,18 +240,4 @@
 </template>
 
 <style>
-  .colon {
-    display: inline-block;
-    margin: 0 4px;
-    color: #1989fa;
-  }
-
-  .block {
-    display: inline-block;
-    width: 22px;
-    color: #fff;
-    font-size: 12px;
-    text-align: center;
-    background-color: #1989fa;
-  }
 </style>
