d109bdcb4f99f9fdde6e85b89c1e71d9d19f05fe
john <john@turingm.io>
2025-09-04 17:52:04 +0800

    query user tasks

--- app/components/Invite/AccountShare.vue
+++ app/components/Invite/AccountShare.vue
@@ -1,28 +1,43 @@
 <script setup>
+import Request from '@/utils/request'
+
 const { userInfo } = $(userStore());
 
-async function onShare() {
-  // const target = document.getElementById('shareTarget');
-  // await captureTargetToPng('shareImageName', target);
-  inviteUser(userInfo.inviteCode)
+onMounted(() => {
+  console.log({ userInfo });
+  loadShareUser();
+})
+
+let shareUserList = $ref([]);
+let totalInvite = $ref(0);
+async function loadShareUser() {
+  try {
+    const rz = await Request({
+      url: `/app-api/topic/user/shareUserPage`,
+      method: 'post',
+      data: {
+        pageNo: 1,
+        pageSize: 12
+      }
+    })
+    console.log('loadShareUser', userInfo.inviteCode, rz);
+
+    if (rz.data.list) {
+      shareUserList = [
+        ...rz.data.list,
+      ];
+      totalInvite = rz.data.total;
+    }
+  } catch (e) {
+    console.error('loadShareUser', e);
+  }
 }
+
+let totalTuit = $ref(0);
 </script>
 
 
 <template>
-  <section class="w-full flex flex-col items-center justify-center space-y-2 p-5">
-    <div id="shareTarget" class="w-full flex flex-col items-center justify-center bg-gray-100 rounded-[12px] p-5 relative">
-      <slot />
-
-      <div class="flex-1 flex flex-col items-center justify-center">
-        <img class="size-30 rounded-full" :src="userInfo.avatar" alt="">
-        <div class="text-center">{{ userInfo.nickname }}</div>
-        <slot name="coin" />
-      </div>
-
-      <div class="flex justify-between items-center w-full space-x-2 mt-12">
-        <van-button block type="primary" @click="onShare">{{ $t("Share") }}</van-button>
-      </div>
-    </div>
-  </section>
+  <InviteShareCard :totalInvite :totalTuit />
+  <InviteUserList :totalInvite :shareUserList />
 </template>
--- app/components/Invite/LeaderBoard.vue
+++ app/components/Invite/LeaderBoard.vue
@@ -1,18 +1,57 @@
 <script setup>
+import Request from '@/utils/request';
+
 const { userInfo } = $(userStore());
+
+let leaderBoard = $ref([]);
+async function loadLeaderBoard() {
+  try {
+    const res = await Request({
+      url: '/app-api/topic/markets/ranks',
+      method: 'POST',
+      data: {
+        type: "share",
+        filterType: 'all',
+        limit: 10
+      }
+    })
+
+    console.log('loadLeaderBoard', res);
+    leaderBoard = res.data;
+  } catch (e) {
+    console.error('loadLeaderBoard', e);
+  }
+}
+
+onMounted(async () => {
+  await loadLeaderBoard();
+})
 </script>
 
 <template>
-  <section class="w-full flex flex-col items-center justify-center bg-white py-2 text-sm">
-    <div class="w-full px-5 grid grid-cols-[2fr_1fr_1fr] justify-center items-center space-y-2 text-gray-700" v-for="n in 10" :key="n">
+  <section class="w-full flex flex-col items-center justify-center bg-white py-2 text-sm px-5">
+    <p class="text-center text-[16px] w-full flex justify-between items-center mb-2">
+      <span>{{ $t('Invite Leaderboard') }}</span>
+      <span class="text-[12px] text-gray-500 mt-2">{{ $t('(Top 10)') }}</span>
+    </p>
+
+    <div class="w-full grid grid-cols-[2fr_1fr_1fr] justify-center items-center space-y-2 text-gray-700">
       <div class="flex justify-start items-center space-x-1 border-0">
-        <img :src="userInfo.avatar" class="rounded-full size-6" alt="">
-        <span>{{ userInfo.nickname }}</span>
+        <span>{{ $t('Rank') }}</span>
       </div>
-      <div class="text-center border-0">50</div>
-      <div class="justify-self-end border-0">700 TUIT</div>
+      <div class="text-center border-0">{{ $t('Referrals') }}</div>
+      <div class="justify-self-end border-0">{{ $t('TUIT') }}</div>
     </div>
 
-    <div class="w-full text-right text-[12px] text-blue-500 pr-4">{{ $t('20th among all user rankings.') }}</div>
+    <div class="w-full grid grid-cols-[2fr_1fr_1fr] justify-center items-center space-y-2 text-gray-700"
+      v-for="(user, i) in leaderBoard" :key="i">
+      <div class="flex justify-start items-center space-x-1 border-0">
+        <span>{{ i + 1 }}.</span>
+        <img :src="user.avatar" class="rounded-full size-4" alt="">
+        <span>{{ user.nickname }}</span>
+      </div>
+      <div class="text-center border-0">{{ user.total }}</div>
+      <div class="justify-self-end border-0">700 TUIT</div>
+    </div>
   </section>
 </template>
