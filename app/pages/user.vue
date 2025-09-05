fb4910d6735b43ec0ebf67a93b37ef665933d2df
Adam <turingmarket@gmail.com>
2025-09-05 17:54:02 +0800

    feat(i18n): add login first prompt for unauthenticated users
    Add "Login First" translation key to all locale files
    Update user page to show login prompt when session is missing

--- app/pages/user.vue
+++ app/pages/user.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-const { session } = $(privyStore());
+  const { session } = $(privyStore());
+  const { setModal } = $(uiStore());
 </script>
 
 <template>
   <div class="h-[calc(100dvh-var(--nav-height))] bg-white overflow-auto">
     <UserInfoCard />
-    <OrderList v-if="session" />
+    <OrderList v-if="!session" />
+    <div v-else class="flex justify-center items-center py-20">
+      <van-button type="success" @click="setModal('loginModal', true)">{{ $t('Login First') }}</van-button>
+    </div>
   </div>
 </template>
-<script setup />
