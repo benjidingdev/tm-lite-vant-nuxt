01bca7f41807468d61f85bc50eb675d82f319769
ben <ben@turingm.io>
2025-09-07 16:11:19 +0800

    Merge branch 'stg-0903-avax' of github.com-work:turingmarket/tm-lite-vant into feature/preparation-for-0908-rollout

--- app/components/Invite/NewUserNotice.vue
+++ app/components/Invite/NewUserNotice.vue
@@ -1,9 +1,31 @@
 <script setup>
+import Request from '@/utils/request';
+
+let newestInvitedUser = $ref([]);
+  async function loadNewestInvitedUser() {
+    try {
+      const res = await Request({
+        url: '/app-api/topic/user/invite/list',
+        method: 'POST',
+        data: {
+        }
+      })
+
+      console.log('loadNewestInvitedUser', res);
+      newestInvitedUser = res.data;
+    } catch (e) {
+      console.error('loadNewestInvitedUser', e);
+    }
+  }
+
+  onMounted(async () => {
+    await loadNewestInvitedUser();
+  })
 
 </script>
 
 <template>
-  <section class="sticky top-0 z-10">
+  <section class="sticky top-0 z-10" v-if="newestInvitedUser.length > 0">
     <van-notice-bar left-icon="volume-o" :scrollable="false">
       <van-swipe vertical class="notice-swipe h-[40px] leading-10" :autoplay="3000" :touchable="false"
         :show-indicators="false">
