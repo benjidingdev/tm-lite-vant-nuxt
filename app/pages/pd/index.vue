6abe8d51040554a262ddd27b389a0b3318d3f614
Adam <turingmarket@gmail.com>
2025-09-07 15:50:50 +0800

    feat(pages): restructure social sharing pages and update translations
    - Move X (Twitter) connection pages from /invite to /pd directory
    - Add new dynamic route pages for user profiles and tasks
    - Fix translation for "Earn" in zh-TW locale
    - Clean up ClaimReward component formatting

--- app/pages/pd/index.vue
+++ app/pages/pd/index.vue
@@ -1,7 +1,7 @@
 <script setup>
-definePageMeta({
-  layout: "x",
-});
+  definePageMeta({
+    layout: "x",
+  });
 </script>
 
 <template>
@@ -27,7 +27,7 @@ definePageMeta({
         </ol>
       </div>
 
-      <NuxtLink to="x/x"
+      <NuxtLink to="/pd/x"
         class="mt-8 w-full rounded-lg bg-green-500 py-3 font-bold text-black flex items-center justify-center space-x-2 transition-transform duration-200 hover:scale-105">
         <svg class="h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm4 11h-3v3h-2v-3H8v-2h3V8h2v3h3z"></path>
