0f438190ce0aceaf04efed2044834ef94f806c1f
ben <ben@turingm.io>
2025-09-10 09:40:16 +0800

    Merge branch 'main' of github.com-work:turingmarket/tm-lite-vant into optimization/enhance-login

--- app/components/RequestQueue/Error.vue
+++ app/components/RequestQueue/Error.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { multiply } from "@/utils/decimal";
+const { t } = useI18n()
 
 
-let { modalIsShow, setModal } = $(uiStore());
-let { failCards } = $(requestQueueStore());
+let { modalIsShow } = $(uiStore());
+let { failCards, requestCount } = $(requestQueueStore());
 
-const close = () => {
-  setModal("requestQueueErrorModal", false);
-};
+function handleRemove(i) {
+  failCards.splice(i, 1);
+  requestCount--;
+}
 </script>
 
 <template>
@@ -16,26 +18,40 @@ const close = () => {
     <article class="w-full h-full flex justify-center items-center flex-col ">
       <div class="w-full text-center text-[18px] py-3 font-[700]">{{ $t("Request Queue Error") }}</div>
 
-      <div class="flex-1 overflow-y-scroll">
-        <template  v-if="failCards.length > 0">
-          <van-swipe-cell v-for="item in failCards" :key="item.marketId">
-            <van-card currency="" :key="item.id" :price="multiply(item.transaction.textPrice || 0, 100) + '€'"
-              :desc="item.transaction.type == 1 ? 'Buy ' : 'Sell '" :title="item.title" :thumb="item.image" class="my-1">
-              <template #footer>
-                <span class="text-red-500">
+      <div class="w-full flex-1 overflow-y-scroll">
+        <template v-if="failCards.length > 0">
+          <van-card v-for="(item, i) in failCards" currency="" :key="item.id"
+            :price="multiply(item.transaction.textPrice || 0, 100) + '€'"
+            :desc="item.transaction.type == 1 ? 'Buy ' : 'Sell '" :title="item.title" :thumb="item.image" class="my-1 w-full">
+            <template #footer>
+              <div class="flex justify-between">
+                <div class="text-red-500 flex-1">
                   {{ item.error?.message || 'Unknown error' }}
-                </span>
-              </template>
-            </van-card>
-            <template #right>
-              <van-button square type="danger" :text="$t('Close')" @click="" />
+                </div>
+                <van-button square size="mini" type="danger" :text="t('Clear')" @click="handleRemove(i)" />
+              </div>
             </template>
-          </van-swipe-cell>
+          </van-card>
         </template>
 
-        <div v-else class="w-full h-full flex items-center">{{ $t('No Error Yet') }}</div>
+        <div v-else class="w-full h-full flex justify-center items-center">{{ $t('No Error Yet') }}</div>
       </div>
 
     </article>
   </van-popup>
 </template>
+
+<i18n lang="json">{
+  "en-US": {
+    "Clear": "Clear"
+  },
+  "zh-TW": {
+    "Clear": "清除"
+  },
+  "ja-JP": {
+    "Clear": "クリア"
+  },
+  "ko-KR": {
+    "Clear": "지우기"
+  }
+}</i18n>
