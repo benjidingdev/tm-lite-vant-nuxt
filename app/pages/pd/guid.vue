a4705bc93375c8cc7650e8b6b3597fb2c468b80d
john <john@turingm.io>
2025-09-14 08:15:40 +0800

    Add Twitter authentication and user profile components; implement API for fetching user data

---
+++ app/pages/pd/guid.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+definePageMeta({
+  layout: "x",
+});
+
+const handleBack = () => {
+  window.history.back()
+}
+</script>
+
+<template>
+  <button class="text-base ml-2 absolute z-999 top-6 border-0" @click="handleBack">
+    <van-icon name="arrow-left" size="30" />
+  </button>
+
+  <PdAuth />
+</template>
