1904e909a740c0561207011549966a3e819a4823
john <john@turingm.io>
2025-09-15 18:39:29 +0800

    update invite guide ui

--- server/api/pd/[uid].ts
+++ server/api/pd/[uid].ts
@@ -11,23 +11,25 @@ export default defineEventHandler(async (event) => {
 
   console.log({ uid })
 
+  let user
   let queryProfile = adminClient
     .from("x_profiles")
     .select(`*`).eq('id', uid).single();
 
-    let queryInvite = adminClient
-      .from("invites")
-      .select(`refCount`).eq('userId', uid).single();
+  let queryInvite = adminClient
+    .from("invites")
+    .select(`refCount`).eq('userId', uid).single();
 
-  const [{error: profileError, data: profileData}, {error: inviteError, data: inviteData}] = await Promise.all([queryProfile, queryInvite]);
+  const [{ error: profileError, data: profileData }, { error: inviteError, data: inviteData }] = await Promise.all([queryProfile, queryInvite]);
   // console.log(profileError, inviteError)
-  if (profileError || inviteError) throw createError({
+  if (profileError) throw createError({
     statusCode: 400,
-    statusMessage: profileError?.message || inviteError?.message
+    statusMessage: profileError?.message
   });
-  return {
-    ...profileData,
-    refCount: inviteData?.refCount,
-  };
+
+  user = profileData
+  user.refCount = inviteData?.refCount || 0;
+
+  return user;
 
 });
