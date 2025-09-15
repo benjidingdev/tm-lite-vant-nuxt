e35dcd2f901da06e43684b296d657172ac8204f1
john <john@turingm.io>
2025-09-15 10:01:37 +0800

    post user refId

--- server/api/proxy/image.post.ts
+++ server/api/proxy/image.post.ts
@@ -1,4 +1,18 @@
 export default defineEventHandler(async (event) => {
   const { url } = await readBody(event)
-  return fetch(url)
+
+  try {
+
+    return $fetch(url, {
+      responseType: 'arraybuffer',
+    }).then(res => {
+      return Buffer.from(res as ArrayBuffer).toString('base64')
+    }).catch(err => {
+      console.log('error', err)
+      return ''
+    })
+  } catch (error) {
+    console.log('error', error)
+    return ''
+  }
 })
