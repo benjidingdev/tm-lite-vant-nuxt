0ddbff30d10d15232c4b1a360585f03d943ee8ea
ben <ben@turingm.io>
2025-09-15 15:21:59 +0800

    enhance earn page and add custom card

--- app/pages/earn.vue
+++ app/pages/earn.vue
@@ -8,11 +8,12 @@ const { setModal } = $(uiStore());
 <template>
   <!--earn dashboard-->
   <div class="h-[calc(100dvh-var(--nav-height))] inset pt-4">
-    <Task class="rounded-xl" v-if="token.accessToken" />
+    <Task v-if="token.accessToken" class="rounded-xl" />
     <div v-else class="flex justify-center items-center py-20">
       <van-button type="success" @click="setModal('loginModal', true)">{{
         $t("Login First")
-      }}</van-button>
+      }}
+      </van-button>
     </div>
   </div>
 </template>
