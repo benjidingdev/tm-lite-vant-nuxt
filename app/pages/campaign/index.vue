ffe565c1809d73fe1f7cc513f518805026360a5a
albert <albertzhang@epclouds.com>
2025-09-16 14:57:21 +0800

    init

---
+++ app/pages/campaign/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+  layout: "order-campaign",
+});
+</script>
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Campaign Page</h1>
+    <p>This is the campaign page content.</p>
+  </div>
+</template>
