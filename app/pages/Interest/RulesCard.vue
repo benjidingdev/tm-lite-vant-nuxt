529468240ddbf537cbd97d37b182b4a035a6a7b0
dailei <hehuan2356@gmail.com>
2025-09-18 18:16:48 +0800

    feat:pledge interest static page completed

---
+++ app/pages/Interest/RulesCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const rules = [
+  '活动需连续打卡，中断则积分清零。',
+  '完成全程返还 20,000 双倍积分。',
+  '成功完成者平分积分池；积分为站内积分，不可提现/转赠。',
+]
+let loading = $ref(true)
+setTimeout(() => {
+  loading = false
+}, 1000)
+</script>
+
+<template>
+  <van-skeleton :loading="loading" animated class="!px-0">
+    <template #template>
+      <div class="w-full rounded-xl bg-white border border-[#f0f0f0] p-4 shadow-sm space-y-2">
+        <div class="h-3 w-20 bg-[#f2f3f5] rounded" />
+        <div class="space-y-2">
+          <div class="h-3 w-[90%] bg-[#f2f3f5] rounded" />
+          <div class="h-3 w-[85%] bg-[#f2f3f5] rounded" />
+          <div class="h-3 w-[80%] bg-[#f2f3f5] rounded" />
+        </div>
+      </div>
+    </template>
+
+    <div class="w-full rounded-xl bg-white border border-[#f0f0f0] p-4 shadow-sm space-y-2">
+    <div class="text-sm text-gray-700">活动规则</div>
+    <ul class="list-disc pl-5 space-y-1 text-xs text-gray-600">
+      <li v-for="(r, i) in rules" :key="i">{{ r }}</li>
+    </ul>
+  </div>
+  </van-skeleton>
+</template>
---
+++ app/pages/Interest/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="w-full h-[calc(100dvh-var(--nav-height))] space-y-px overflow-y-scroll">
+    <div class="px-4 flex flex-col space-y-4 pb-4">
+      <Notice />
+      <KpiCards />
+      <TaskCard />
+      <ShareInvite />
+      <LeaderboardTabs />
+      <RulesCard />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import TaskCard from './TaskCard.vue'
+import KpiCards from './KpiCards.vue'
+import ShareInvite from './ShareInvite.vue'
+import LeaderboardTabs from './LeaderboardTabs.vue'
+import RulesCard from './RulesCard.vue'
+import Notice from './Notice.vue'
+
+</script>
+
