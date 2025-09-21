20f3c56d3a594fc1b8fbd67cafef0de382102e47
Adam <turingmarket@gmail.com>
2025-09-21 15:35:37 +0800

    refactor(rank): replace van-image with XAvatar component
    Simplify avatar rendering by using the new XAvatar component and update user profile links

---
+++ app/components/XAvatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<template>
+  <van-image class="w-full h-full bg-cover" :src="xAvatar(src)">
+    <template v-slot:loading>
+      <van-loading type="spinner" size="20" />
+    </template>
+  </van-image>
+</template>
