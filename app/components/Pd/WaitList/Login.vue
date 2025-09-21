ca4f2c40e6fd6cf25ca05ac7d9139e731662fc7d
john <john@turingm.io>
2025-09-21 11:44:00 +0800

    update p-[pid] page width responsive

--- app/components/Pd/WaitList/Login.vue
+++ app/components/Pd/WaitList/Login.vue
@@ -10,12 +10,12 @@ async function handleLogin() {
 </script>
 
 <template>
-  <section class="mt-20 w-full flex flex-col items-center justify-center space-y-4 p-4 rounded-md">
+  <section class="mt-20 w-full flex flex-col items-center justify-center space-y-4 py-4 rounded-md">
     <button class="w-full bg-blue-500 text-white px-4 py-2 rounded-[8px] bg-[#7000FF]" @click="handleLogin">
       Authorize X account
     </button>
 
-    <p class="text-center text-gray-500 text-xs mt-10">
+    <p class="text-center text-gray-500 text-xs mt-0">
       Join the TuringM Prediction Master waitlist and get early access to the
       platform.
     </p>
