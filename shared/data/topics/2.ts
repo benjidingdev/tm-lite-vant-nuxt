e8955beab95e431df961dd8b61846908e588e81b
john <john@turingm.io>
2025-09-22 19:42:14 +0800

    add topic 2

---
+++ shared/data/topics/2.ts
@@ -0,0 +1,22 @@
+export default {
+  title: 'checkin',
+  meta: {
+    status: 'waiting', // started, ended
+    "logo": "/topic/predmoon.png",
+    "shareBg": "/topic/monad-bg.jpg",
+    "shareLogo": "/topic/shareLogo.png",
+    shareTitle: 'Pred to the moon',
+    "x_info": {
+      "hashtags": "TuringM,TuringMaster,Airdrop",
+      "retweetTargetLink": "https://x.com/TuringMarket/status/1958786009753428017",
+      "text": "Turing Market has a topic of {{title}} ,Claim the reward via {{url}}",
+      shareRankText: "Turing Market has a topic of {{title}} ,Share rank wall to Claim the reward via {{url}}",
+    },
+    "rewards": {
+      "auth": 1000,
+      "invite": 1000,
+      "retweet": 100
+    }
+  },
+  markets: []
+}
