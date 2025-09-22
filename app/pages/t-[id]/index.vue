2b2de3bfd8a5735e667c3220b3f900448b2a4651
Adam <turingmarket@gmail.com>
2025-09-22 11:43:53 +0800

    feat(composables): add useLinkTo for localized routing
    refactor(pages): restructure topic page layout and add back link

--- app/pages/t-[id]/index.vue
+++ app/pages/t-[id]/index.vue
@@ -82,16 +82,19 @@ onMounted(() => {
         </div>
       </template>
 
-      <img :src="topic?.meta?.logo" alt="" class="w-30 mt-10">
-      <p class="text-[30px] font-900 mb-10 leading-[1.2] py-2 text-center">{{ topic?.title }}</p>
-      <!-- <h2 class="text-lg font-bold">{{ hasRetweeted ? 'You are on the Waitlist' : 'Join the Waitlist' }}</h2> -->
+      <NuxtLink :to="useLinkTo('/')" class="w-10 h-10 absolute top-2 left-2 bg-gray-400">
+        <xAvatar src="/icons/market.svg" />
+      </NuxtLink>
+      <div class="w-full flex flex-col justify-center items-center">
+        <img :src="topic?.meta?.logo" alt="" class="w-30 mt-10">
+        <p class="text-[30px] font-900 mb-10 leading-[1.2] py-2 text-center">{{ topic?.title }}</p>
+      </div>
 
       <template v-if="hasTwitterLogin">
         <div v-if="topic?.meta?.isWaitingClosed" class="w-full h-full flex flex-col justify-center items-center mt-6">
           <SwipeCardPDC :topic />
         </div>
         <TopicWaitListRetweet :topic v-model="hasRetweeted" @onSuccess="() => { refreshTime = new Date() }" />
-
       </template>
 
       <TopicWaitListLogin v-else />
